57726d341678a04a958bce2f87c5f19390546ad7
Victor Poletti <victorpoletti2009@hotmail.com>
2019-07-10 20:36:16 -0300

    Criando usuario no banco com password_hash

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Victor Poletti',
-    email: 'victor@gotmail.com',
-    password_hash: 'akljdfhkajlsfhld',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
