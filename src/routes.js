61561276231d73187ca1f4d3b6812c60e5c79e1f
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-07-10 16:56:10 -0300

    Configurando sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'salveee' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Victor Poletti',
+    email: 'victor@gotmail.com',
+    password_hash: 'akljdfhkajlsfhld',
+  });
+  return res.json(user);
 });
 
 export default routes;
