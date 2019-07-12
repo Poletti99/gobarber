e7782f5d06a44e8fecc45dd21b238db613b113a7
Victor Poletti <victor.poletti@gastecnologia.com.br>
2019-07-12 15:57:20 -0300

    Enviando email ao cancelar agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8356a5b0c0eced',
+    pass: 'd00f92ca456a7d',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
